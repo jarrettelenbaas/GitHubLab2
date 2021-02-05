68aa2e887d500105860b669bbc36254d30e80c76
Jarrett Elenbaas <jelenbaas1@carthage.edu>
2021-02-05 14:07:58 -0600

    Added a square function

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+void square(int num);
+
 int main(){
     cout << "GitHub Test" << endl;
+    square(1);
+    square(5);
+    square(10);
+    square(-1);
+    square(0);
 
     return 0;
+}
+
+void square(int num){
+    cout << (num * num) << endl;
 }
